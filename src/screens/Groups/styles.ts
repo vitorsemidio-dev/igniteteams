d78e2fa5407a652e042536179b467924a2780994
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 15:49:40 -0300

    feat(groups): create list empty component

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  align-items: center;
   padding: 24px;
 `;
 
