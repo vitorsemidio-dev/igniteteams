b90f6dc6461b77071b729b934ce5b4f367a629df
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 11:48:55 -0300

    feat(fonts): add roboto font to project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <StatusBar style="auto" />
-        <Groups />
+        {fontsLoaded ? <Groups /> : <ActivityIndicator />}
       </ThemeProvider>
     </>
   );
