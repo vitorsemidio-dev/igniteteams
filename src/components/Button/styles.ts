4eb99a0009e38ee008269a001077f585daa81d23
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 16:23:05 -0300

    feat(button): create button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,36 @@
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+  type: ButtonTypeStyleProps;
+};
+
+export const Container = styled(TouchableOpacity)<Props>`
+  flex: 1;
+
+  min-height: 56px;
+  max-height: 56px;
+
+  background-color: ${({ theme, type }) => {
+    switch (type) {
+      case 'PRIMARY':
+        return theme.COLORS.GREEN_700;
+      case 'SECONDARY':
+        return theme.COLORS.RED_DARK;
+      default:
+        return theme.COLORS.GRAY_500;
+    }
+  }};
+
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`;
