8b8e4807c8c512352bd13dcb4c9c89d9bd2a6d27
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 12:57:32 -0300

    feat(icons): install icon lib (phosphor-react-native)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from './styles';
 import logoImg from '@assets/logo.png';
+import { BackButton, BackIcon, Container, Logo } from './styles';
 
-export function Header() {
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   );
