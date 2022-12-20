a099279e7829821efedec22ae893c4bc9152aa2f
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 12:41:46 -0300

    feat(header): create header component with logo

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,7 +4,6 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   align-items: center;
-  justify-content: center;
 `;
 
 export const Title = styled.Text`
