bf8403c114ffa417b8ca8a0786e5815359994df4
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-20 11:30:54 -0300

    feat(theme): setup app theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <Groups />
+      <ThemeProvider theme={theme}>
+        <StatusBar style="auto" />
+        <Groups />
+      </ThemeProvider>
     </>
   );
 }
